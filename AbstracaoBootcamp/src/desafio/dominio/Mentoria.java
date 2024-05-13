b4379be3295a1fed4e4eb3a7184e8a292fca55b8
devlavanere <michel.lavanere@academico.ifpb.edu.br>
2024-05-13 00:33:24 -0300

    Update projeto

---
+++ AbstracaoBootcamp/src/desafio/dominio/Mentoria.java
@@ -0,0 +1,29 @@
+package desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo {
+	
+	LocalDate data;
+	
+	public Mentoria() {
+	}
+	
+	@Override
+	public double calcularXp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	@Override
+	public String toString() {
+		return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
+	}
+	
+}
